d31e8523e82bdfaac7e40c56c397165b79ecea84
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2014-10-27 15:19:41 -0400

    Highlight return matches

--- app/controllers/regex.js
+++ app/controllers/regex.js
@@ -1,18 +1,18 @@
 import Ember from "ember";
 
-export default Ember.ObjectController.extend({
+var RegexController = Ember.ObjectController.extend({
   actions: {
     match: function(){
-      var url = "http://mock.mocaroni.com/Wy5nuHes4mf34C79p_AH9v4aSPA/match?status=200"
-      $.ajax({
-        url: url,
-        data: {},
-        dataType: 'json',
-        success: function(response){
-
-        }
-      })
+      var self = this;
+      $.post(self.url, {}, function(response){
+          self.model.highlightMatches(response.matches)
+        }, 'json'
+      )
     }
-  }
+  },
+
+  url: "http://mock.mocaroni.com/Wy5nuHes4mf34C79p_AH9v4aSPA/match?status=200"
 
 });
+
+export default RegexController;
--- app/models/regex.js
+++ app/models/regex.js
@@ -1,7 +1,24 @@
 import Ember from "ember";
 
 var Regex = Ember.Object.extend({
-  editorText: "hello there"
+  editorText: "hello there with all the matches",
+
+  highlightMatches: function(matches){
+    this.set('matches', matches)
+    this.set('editorText', this.highlights())
+  },
+
+  highlights: function(){
+    var highlighted_text =  this.get('editorText').replace(this.pattern(), function(matchString){
+      return "<span>" + matchString + "</span>"
+    })
+    return Ember.String.htmlSafe(highlighted_text)
+  },
+
+  pattern: function(){
+    return new RegExp("(" + this.matches.join(")|(") + ")", "g")
+  },
+
 });
 
 export default Regex;
